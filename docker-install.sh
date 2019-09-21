542af28996bd6f29754925294fe9a9c237adedae
Deusimar Ferreira <deusimardosanjos@gmail.com>
2019-09-21 15:46:17 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Referência
+# https://docs.docker.com/v17.09/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
+apt-get update
+
+apt-get install apt-transport-https ca-certificates curl software-properties-common net-tools
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+
+apt-get update
+
+apt-get install docker-ce
