bbae3390e6eaa65b83a4fe6780279849619365ca
Deusimar Ferreira <deusimardosanjos@gmail.com>
2020-04-18 10:16:08 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -2,14 +2,14 @@
 
 # Referência
 # https://docs.docker.com/v17.09/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
-apt-get update
+sudo apt-get update
 
-apt-get install apt-transport-https ca-certificates curl software-properties-common net-tools
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common net-tools gnupg-agent
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
 
-apt-get update
+sudo apt-get update
 
-apt-get install docker-ce
+sudo apt-get install docker-ce
