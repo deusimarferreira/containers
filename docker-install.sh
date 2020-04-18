7e7d3284173dfee1fa46430ecbf0b66d4a3eb661
Deusimar Ferreira <deusimardosanjos@gmail.com>
2020-04-18 10:29:59 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -6,6 +6,7 @@ sudo apt-get update
 
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common net-tools gnupg-agent
 
+# Recomendo executar esse comando as root
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
