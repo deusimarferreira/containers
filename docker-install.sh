4379e2b60744563987cec786cc47ada8add02034
Deusimar Ferreira <deusimardosanjos@gmail.com>
2023-03-06 16:54:06 -0300

    Atualizacao do script

--- docker-install.sh
+++ docker-install.sh
@@ -5,13 +5,26 @@
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common net-tools gnupg-agent
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt install apt-transport-https ca-certificates curl software-properties-common net-tools gnupg lsb-release
 
 # Recomendo executar esse comando as root
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
-
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 sudo apt update
+#apt-cache policy docker-ce
 
-sudo apt -y install docker-ce
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl status docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#su - ${USER}
+#groups
+newgrp docker
